ac45e0c53488306942c61b1488e4c7ac5a507173
askinekinci <askinekincihf@gmail.com>
2021-05-05 14:57:24 +0100

    completed Level-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import React, { useState } from "react";
 import './App.css';
 import BabyNamesData from "./data/BabyNamesData.json";
 import BabyNamesCard from "./BabyNamesCard"
+import Search from "./Search"
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value);
+  }
+
   const babyNames = BabyNamesData.sort((previousName, currentName) => previousName.name.localeCompare(currentName.name));
 
-  console.log(babyNames);
+  const filteredBabyNames = babyNames.filter(baby => baby.name.toUpperCase().includes(searchInput.toUpperCase()))
 
   return (
     <div>
       <div className="name-container">
-        {babyNames.map((babyName, index) => (
-          <BabyNamesCard key={index} babyName={babyName} index={index} />
+        <Search
+          babyNames={babyNames}
+          searchInput={searchInput}
+          handleSearchChange={handleSearchChange}
+        />
+        {filteredBabyNames.map((babyName, index) => (
+          <BabyNamesCard
+            key={index}
+            babyName={babyName}
+            index={index}
+          />
         ))}
       </div>
     </div>
--- src/BabyNamesCard.js
+++ src/BabyNamesCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BabyNamesCard = ({ babyName, index }) => {
+const BabyNamesCard = ({ babyName }) => {
     return (
         <div>
             <p className={babyName.sex === "f" ? "name-female" : "name-male"}>{babyName.name}</p>
